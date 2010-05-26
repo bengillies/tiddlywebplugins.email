e9faf6025103bf92275407c4bb17441ab3fd7929
bengillies <bengillies@gmail.com>
2010-05-26 20:09:12 +0100

    changed import to named imports

--- tiddlywebplugins/mail/__init__.py
+++ tiddlywebplugins/mail/__init__.py
@@ -2,7 +2,7 @@
 entry point for tiddlywebplugins.mail
 """
 from config import config as email_config
-from twanager import *
+from twanager import send_subscriptions, slurp_email, check_email
 
 from tiddlyweb.manage import merge_config
 
